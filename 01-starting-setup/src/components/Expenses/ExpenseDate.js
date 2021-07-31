28f831a0875bae4d528dce88f40997e9d729fe5a
Taylor Smith <tas0018@auburn.edu>
2021-07-30 19:51:43 -0500

    finished lecture 57

--- 01-starting-setup/src/components/Expenses/ExpenseDate.js
+++ 01-starting-setup/src/components/Expenses/ExpenseDate.js
@@ -9,7 +9,7 @@ function ExpenseDate(props) {
     <div className="expense-date">
       <div className="expense-date__month">{month}</div>
       <div className="expense-date__day">{day}</div>
-      <div classNamw="expense-date__year">{year}</div>
+      <div className="expense-date__year">{year}</div>
     </div>
   );
 }
--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,27 @@
+import React, { useState } from "react";
+
 import ExpenseDate from "../Expenses/ExpenseDate";
 import "../Expenses/ExpenseItem.css";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  // ClickHandler function
+
+  const clickHandler = () => {
+    setTitle("updated");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
