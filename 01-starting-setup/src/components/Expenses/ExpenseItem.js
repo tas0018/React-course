bba5003693f585716ccd30375a9c9fb6c5d31c04
Taylor Smith <tas0018@auburn.edu>
2021-08-18 16:21:48 -0500

    finished lessons up to 63. I need to review react state again

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
 import ExpenseDate from "../Expenses/ExpenseDate";
 import "../Expenses/ExpenseItem.css";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-
-  // ClickHandler function
-
-  const clickHandler = () => {
-    setTitle("updated");
-    console.log(title);
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
